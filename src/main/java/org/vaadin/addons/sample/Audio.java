460657ad257d47954083f87a45b2a4495034d0bc
mukherjeesudebi <sudebi@vaadin.com>
2024-08-12 15:51:17 +0300

    added visualizer

--- src/main/java/org/vaadin/addons/sample/Audio.java
+++ src/main/java/org/vaadin/addons/sample/Audio.java
@@ -1,11 +1,37 @@
 package org.vaadin.addons.sample;
 
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.template.Id;
 
 @Tag("audio-element")
 @JsModule("./audio-element.ts")
-public class Audio extends Component{
+public class Audio extends LitTemplate{
+	 
+	@Id("startButton")
+    private Button startButton;
 
+    @Id("stopButton")
+    private Button stopButton;
+    
+    public Audio() {
+    	stopButton.setVisible(false);
+    	startButton.addClickListener(event -> {
+    		startButton.setVisible(false);
+    		stopButton.setVisible(true);
+    	});
+    	stopButton.addClickListener(event -> {
+    		startButton.setVisible(true);
+    		stopButton.setVisible(false);
+    	});
+    	
+    }
+
+    @ClientCallable
+    public void saveRecordedAudio(String audio) {
+        System.out.println(audio.toString());
+    }
 }
--- src/main/resources/META-INF/resources/frontend/audio-element.ts
+++ src/main/resources/META-INF/resources/frontend/audio-element.ts
@@ -4,85 +4,157 @@ import { property, customElement, query } from 'lit/decorators.js';
 @customElement('audio-element')
 export class AudioElement extends LitElement {
 
-    recordingTimeMS = 5000;
+	recordingTimeMS = 5000;
 
-    @query('#preview')
-    _preview;
+	@query('#preview')
+	_preview;
 
-    @query('#recording')
-    _recording;
+	@query('#recording')
+	_recording;
 
-    record(stream, lengthInMS) {
-        let recorder = new MediaRecorder(stream);
-        let data: Blob[] = [];
+	@query('#visualizer')
+	_visualizer
 
-        recorder.ondataavailable = (event) => data.push(event.data);
-        recorder.start();
+	record(stream, lengthInMS) {
+		let recorder = new MediaRecorder(stream);
+		let data: Blob[] = [];
 
-        let stopped = new Promise((resolve, reject) => {
-            recorder.onstop = resolve;
-            recorder.onerror = (event) => reject(event.currentTarget);
-        });
+		recorder.ondataavailable = (event) => data.push(event.data);
+		recorder.start();
 
-        let recorded = this.wait(lengthInMS).then(() => {
-            if (recorder.state === "recording") {
-                recorder.stop();
-            }
-        });
+		let stopped = new Promise((resolve, reject) => {
+			recorder.onstop = resolve;
+			recorder.onerror = (event) => reject(event.currentTarget);
+		});
 
-        return Promise.all([stopped, recorded]).then(() => data);
-    }
+		let recorded = this.wait(lengthInMS).then(() => {
+			if (recorder.state === "recording") {
+				recorder.stop();
+			}
+		});
 
-    startRecording() {
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true,
-        })
-            .then((stream) => {
-                this._preview.srcObject = stream;
-               // this._preview.captureStream = this._preview.captureStream || this._preview.mozCaptureStream;
-                return new Promise((resolve) => (this._preview.onplaying = resolve));
-            }).then(() => this.record(this._preview.captureStream(), this.recordingTimeMS))
-            .then((recordedChunks) => {
-                let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
-                this._recording.src = URL.createObjectURL(recordedBlob);
-            }).catch((error) => {
-                if (error.name === "NotFoundError") {
-                    console.log("Microphone not found. Can't record.");
-                } else {
-                    console.log(error);
-                }
-            })
-    }
+		return Promise.all([stopped, recorded]).then(() => data);
+	}
 
-    stopRecording() {
-        this.stop(this._preview.srcObject);
-    }
+	async createVisualizer(stream,canvasCtx) {
+		var WIDTH = 300;
+		var HEIGHT = 53;
+		var audioContext = new AudioContext();
+		var analyserNode = audioContext.createAnalyser();
+		var source = audioContext.createMediaStreamSource(stream);
+		source.connect(analyserNode);
+		analyserNode.fftSize = 2048;
+		var bufferLength = analyserNode.frequencyBinCount;
+		var dataArray = new Uint8Array(bufferLength);
+		//var canvasCtx = this._viualizer.getContext("2d");
+		//canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    wait(delayInMS) {
-        return new Promise((resolve) => setTimeout(resolve, delayInMS));
-    }
+		const draw = () => {
+			requestAnimationFrame(draw);
 
-    stop(stream) {
-        stream.getTracks().forEach((track) => track.stop());
-    }
+        analyserNode.getByteFrequencyData(dataArray);
 
-    render(): TemplateResult {
-        return html`
-        <vaadin-horizontal-layout theme="spacing" class="items-center">
-            <vaadin-button theme="icon" id="startButton" @click="${this.startRecording}">
-                <vaadin-icon icon="vaadin:play-circle-o"></vaadin-icon>
-            </vaadin-button>
-            <audio id="preview" width="160" height="120" autoplay muted></audio>
-            <vaadin-button theme="icon" id="stopButton" @click="${this.stopRecording}">
-                <vaadin-icon icon="vaadin:stop"></vaadin-icon>
-            </vaadin-button>
-            <audio id="recording" width="160" height="120" controls></audio>	
-        </vaadin-horizontal-layout>  
-        `;
-    }
+        canvasCtx.fillStyle = "rgb(0, 0, 0)";
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        const barWidth = (WIDTH / bufferLength) * 2.5;
+        let x = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+          const barHeight = dataArray[i];
+
+          canvasCtx.fillStyle = "rgb(" + (barHeight + 100) + ",50,50)";
+          canvasCtx.fillRect(
+            x,
+            HEIGHT - barHeight / 2,
+            barWidth,
+            barHeight / 2
+          );
+
+          x += barWidth + 1;
+        }
+		}
+
+
+		draw();
+
+
+	}
+	
+	startRecording() {
+		navigator.mediaDevices.getUserMedia({
+			video: true,
+			audio: true,
+		})
+			.then((stream) => {
+				this._preview.srcObject = stream;
+				this.createVisualizer(stream,this._visualizer.getContext("2d"));
+				// this._preview.captureStream = this._preview.captureStream || this._preview.mozCaptureStream;
+				return new Promise((resolve) => (this._preview.onplaying = resolve));
+			}).then(() => this.record(this._preview.captureStream(), this.recordingTimeMS))
+			.then((recordedChunks) => {
+				let recordedBlob = new Blob(recordedChunks, { type: "audio/ogg; codecs=opus" });
+				const audioURL = URL.createObjectURL(recordedBlob)
+				this._recording.src = audioURL;
+				this.saveRecordedAudio(recordedBlob);
+			}).catch((error) => {
+				if (error.name === "NotFoundError") {
+					console.log("Microphone not found. Can't record.");
+				} else {
+					console.log(error);
+				}
+			})
+	}
+
+	async saveRecordedAudio(recordedBlob) {
+		let reader = new FileReader();
+		let base64String;
+		reader.onload = function () {
+			base64String = reader.result;
+		}
+		reader.readAsDataURL(recordedBlob);
+		setTimeout(() => {
+			this.$server.saveRecordedAudio(base64String);
+		}, 500);
+	}
+
+	stopRecording() {
+		this.stop(this._preview.srcObject);
+	}
+
+	wait(delayInMS) {
+		return new Promise((resolve) => setTimeout(resolve, delayInMS));
+	}
+
+	stop(stream) {
+		stream.getTracks().forEach((track) => track.stop());
+	}
+
+	static get styles() {
+		return css`
+
+`
+	}
+
+	render(): TemplateResult {
+		return html`
+<vaadin-vertical-layout theme="spacing" class="items-center">
+	<vaadin-horizontal-layout>
+		<vaadin-button theme="tertiary" id="startButton" @click="${this.startRecording}">
+			<vaadin-icon icon="vaadin:microphone"></vaadin-icon>
+		</vaadin-button>
+		<vaadin-button theme="tertiary" id="stopButton" style="color: red" @click="${this.stopRecording}">
+			<vaadin-icon icon="vaadin:stop"></vaadin-icon>
+		</vaadin-button>	
+	<audio id="preview" width="160" height="120" autoplay muted></audio>
+	<canvas id="visualizer" width="300" height="50"></canvas>
+	<audio id="recording" width="160" height="120" controls></audio>
+	</vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
+	}
 }
 
 function log(arg0: string) {
-    throw new Error('Function not implemented.');
+	throw new Error('Function not implemented.');
 }
